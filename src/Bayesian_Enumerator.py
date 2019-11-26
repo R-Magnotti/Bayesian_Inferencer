027d6742053ca583e326129ce3cbe7769f8ba1e6
Masane <richard.magnotti@gmail.com>
2019-11-26 01:15:51 -0500

    Exact inference, condition: currentVar in evidence - now works.

--- src/Bayesian_Enumerator.py
+++ src/Bayesian_Enumerator.py
@@ -17,15 +17,53 @@ def enumerateAsk(X, evidence, bn, varList):
 def enumerateAll(bn, e, vars):
     if vars == []:
         return 1
-    print('vars[0] ', vars[0], ' and e.keys ', e.keys())
+
+    print(vars[0])
+    #if current variable from list is in the evidence
     if vars[0] in e.keys():
+        truthValueList = []
+
         # get the value of y that is in the evidence
         y = (vars[0], e[vars[0]])
-        print('y value ', y)
+
+        #XML order of probabilities is reversed
+        #add truth value in REVERSE, so T = 0, F = 1
+        if y[1] is True:
+            truthValueList.append('0')
+        else:
+            truthValueList.append('1')
+
+        #grab parents of y
+        parentsY = []
+        currQueryNode = None
+        for item in bn:
+            if item.name == y[0]:
+                currQueryNode = item
+                parentsY = item.parent
+                break
+
+        #grab truth values of parents of y
+        for item in parentsY:
+            #to make sure to get rid of root node from probability distribution
+            if item.name == 'root':
+                break
+            # XML order of probabilities is reversed
+            # add truth value in REVERSE, so T = 0, F = 1
+            if item.value is True:
+                truthValueList.append('0')
+            else:
+                truthValueList.append('1')
+        print('truth value list ', truthValueList)
+
+        #grab the decimal representation of this binary number
+        decVal = int(''.join(truthValueList), 2)
+
+        #grab the decVal-th element from the list
+        ithProbability = currQueryNode.probs[decVal]
+        print(ithProbability)
 
     else:
         pass
-
 
 # global scope variables from the xml parser
 def main():
@@ -36,11 +74,13 @@ def main():
     networkList = xml.make_network(root, test, start)
 
     # topologically ordered list of nodes
-    # y[0] is the root node of the graph which represents the entire graph
     postOrderNodes = xml.linearize(networkList)
-    postOrderList = xml.get_var_names(postOrderNodes)
-    postOrderList = postOrderList[1:]
+    postOrderNodesCut = postOrderNodes[1:]  # the entire list of nodes minus the root node
 
-    enumerateAsk('B', {'B':True, 'C':False}, postOrderNodes, postOrderList)
+    # postOrderList is JUST A LIST OF NODE NAMES, NOT NODE OBJECTS
+    postOrderList = xml.get_var_names(postOrderNodes)
+    postOrderListCut = postOrderList[1:] #the entire list of nodes minus the root node
+
+    enumerateAsk('B', {'B':True, 'C':False}, postOrderNodesCut, postOrderListCut)
 
 main()
